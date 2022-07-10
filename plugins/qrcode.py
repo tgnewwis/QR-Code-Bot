2056776b06fed4cf2a1902162eeac0b9190303ca
tgnewwis <108725170+tgnewwis@users.noreply.github.com>
2022-07-10 18:58:30 +0530

    Update qrcode.py

--- plugins/qrcode.py
+++ plugins/qrcode.py
@@ -9,7 +9,7 @@ from database import db
 
 QR_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url=f"https://telegram.me/FayasNoushad")
+        InlineKeyboardButton(text="🚀 Join Updates Channel 🚀", url=f"https://t.me/EpicBotsSl")
         ]]
     )
 
@@ -18,18 +18,18 @@ QR_BUTTONS = InlineKeyboardMarkup(
 async def qr_decode(bot, update):
     if not await db.is_user_exist(update.from_user.id):
         await db.add_user(update.from_user.id)
-    decode_text = await update.reply_text(text="<b>Processing your request...</b>")
+    decode_text = await update.reply_text(text="<b>😊Processing your request...</b>")
     dl_location = str(update.from_user.id)
     im_dowload = ''
     qr_text = ''
     try:
-        await decode_text.edit("Trying to download....")
+        await decode_text.edit("🙄Trying to download....")
         im_dowload = await update.download(file_name=dl_location + '.png')
     except Exception as error:
         await decode_text.edit(text=error)
         return
     try:
-        await decode_text.edit(text="Decoding.....")
+        await decode_text.edit(text="🙃Decoding.....")
         qr_text_data = decode(Image.open(im_dowload))
         qr_text_list = list(qr_text_data[0])  # Listing
         qr_text_ext = str(qr_text_list[0]).split("'")[1]  # Text Extract
@@ -38,9 +38,9 @@ async def qr_decode(bot, update):
         await decode_text.edit(text=error)
         return
     await decode_text.edit_text(
-        text=f"Link :- {qr_text}\n\nMade by @FayasNoushad",
+        text=f"Link :- {qr_text}\n\nMade by @wisula4 🤭",
       	reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url=f"https://telegram.me/FayasNoushad")]]
+            [[InlineKeyboardButton(text="🚀 Join Updates Channel 🚀", url=f"https://t.me/EpicBotsSl")]]
         ),
         disable_web_page_preview=True
     )
@@ -55,7 +55,7 @@ async def qr_encode(bot, update):
     if not await db.is_user_exist(update.from_user.id):
         await db.add_user(update.from_user.id)
     qr = await update.reply_text(
-        text="Making your QR Code...",
+        text="😎Making your QR Code...",
         quote=True
     )
     s = str(update.text)
@@ -65,7 +65,7 @@ async def qr_encode(bot, update):
     img = qrname + '.png'
     as_file = await db.is_as_file(update.from_user.id)
     try:
-        await qr.edit_text("Trying to Uploading....")
+        await qr.edit_text("📤Trying to Upload....")
         if as_file:
             await update.reply_document(
                 document=img,
