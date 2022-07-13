d99cf51b07c4e854ca772d7ca2d11d49885e95bc
tgnewwis <108725170+tgnewwis@users.noreply.github.com>
2022-07-13 11:17:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ Bot = Client(
     bot_token=os.environ.get("BOT_TOKEN"),
     api_id=int(os.environ.get("API_ID")),
     api_hash=os.environ.get("API_HASH"),
+    mongodb_uri=(os.environ.get.("MONGODB_URI")
     plugins=dict(root="plugins")
 )
 
