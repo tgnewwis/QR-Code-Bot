25399e2423eda2f1d48126ffe25caabb287127ab
tgnewwis <108725170+tgnewwis@users.noreply.github.com>
2022-07-13 11:28:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from pyrogram import Client
 
 load_dotenv()
 
-Bot = Client(
+Bot = Client()
     "QR Code Bot",
     bot_token=os.environ.get("BOT_TOKEN"),
     api_id=int(os.environ.get("API_ID")),
     api_hash=os.environ.get("API_HASH"),
     mongodb_uri=(os.environ.get.("MONGODB_URI")
     plugins=dict(root="plugins")
-)
+
 
 Bot.run()
